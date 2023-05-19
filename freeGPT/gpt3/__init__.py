fa52dc754c064670414448c0b142a22685fc1c47
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-05-19 22:39:38 +0530

    Update __init__.py

--- freeGPT/gpt3/__init__.py
+++ freeGPT/gpt3/__init__.py
@@ -1,16 +1,12 @@
 import os, re, json
-os.system("pip install tls-client --no-cache-dir")
+try:
+    from tls_client import Session
+except:
+    os.system("pip install tls_client --no-cache-dir")
 from uuid import uuid4
 from pydantic import BaseModel
-from tls_client import Session
 from fake_useragent import UserAgent
 from typing import Optional, List, Dict, Any
-
-
-class PoeResponse(BaseModel):
-    text: Optional[str] = None
-    links: List[str] = []
-    extra: Dict[str, Any] = {}
 
 
 class Completion:
@@ -28,13 +24,12 @@ class Completion:
         chat: list = None,
         include_links: bool = False,
         detailed: bool = False,
-        debug: bool = False,
         proxy: Optional[str] = None
-    ) -> PoeResponse:
+    ) -> dict:
         if chat is None:
             chat = []
 
-        proxies = { 'http': 'http://' + proxy, 'https': 'http://' + proxy } if proxy else {}
+        proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy} if proxy else {}
 
         client = Session(client_identifier='chrome_108')
         client.headers = Completion.__get_headers()
@@ -52,17 +47,12 @@ class Completion:
                 'responseFilter': response_filter,
                 'domain': domain,
                 'queryTraceId': str(uuid4()) if query_trace_id is None else query_trace_id,
-                'chat': str(chat),  # {'question':'','answer':' ''}
+                'chat': str(chat),
             },
         )
 
-        if debug:
-            print('\n\n------------------\n\n')
-            print(response.text)
-            print('\n\n------------------\n\n')
-
         if 'youChatToken' not in response.text:
-            return Completion.__get_failure_response()
+            raise Exception('Unable to get the response, please try again later.')
 
         you_chat_serp_results = re.search(
             r'(?<=event: youChatSerpResults\ndata:)(.*\n)*?(?=event: )', response.text
@@ -70,23 +60,24 @@ class Completion:
         third_party_search_results = re.search(
             r'(?<=event: thirdPartySearchResults\ndata:)(.*\n)*?(?=event: )', response.text
         ).group()
-        # slots                   = findall(r"slots\ndata: (.*)\n\nevent", response.text)[0]
 
-        text = ''.join(re.findall(r'{\"youChatToken\": \"(.*?)\"}', response.text))
+        text = ''.join(re.findall(r'{"youChatToken": "(.*?)"}', response.text))
 
         extra = {
             'youChatSerpResults': json.loads(you_chat_serp_results),
-            # 'slots'                   : loads(slots)
         }
 
-        response = PoeResponse(text=text.replace('\\n', '\n').replace('\\\\', '\\').replace('\\"', '"'))
+        result = {
+            'text': text.replace('\\n', '\n').replace('\\\\', '\\').replace('\\"', '"')
+        }
+
         if include_links:
-            response.links = json.loads(third_party_search_results)['search']['third_party_search_results']
+            result['links'] = json.loads(third_party_search_results)['search']['third_party_search_results']
 
         if detailed:
-            response.extra = extra
+            result['extra'] = extra
 
-        return response
+        return result
 
     @staticmethod
     def __get_headers() -> dict:
@@ -106,6 +97,4 @@ class Completion:
             'user-agent': UserAgent().random,
         }
 
-    @staticmethod
-    def __get_failure_response() -> PoeResponse:
-        return PoeResponse(text='Unable to fetch the response, Please try again.')
+raise Exception('Unable to get the response, please try again later.')
