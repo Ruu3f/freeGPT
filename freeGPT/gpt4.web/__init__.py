017b288b0a74383e62f4155d9852413d10431e13
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-05-25 16:04:33 +0530

    Update __init__.py

--- freeGPT/gpt4.web/__init__.py
+++ freeGPT/gpt4.web/__init__.py
@@ -1,5 +1,5 @@
 import os, re, json
-from pydantic import BaseModel
+
 try:
     from tls_client import Session
 except ImportError:
@@ -7,6 +7,7 @@ except ImportError:
 from uuid import uuid4
 from requests import post
 from time import time, sleep
+from pydantic import BaseModel
 from fake_useragent import UserAgent
 from pymailtm import MailTm, Message
 from typing import Any, List, Generator, Optional
@@ -48,53 +49,58 @@ class Account:
         Returns:
             str: The token associated with the created account.
         """
-        proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy} if proxy else False
+        proxies = (
+            {"http": "http://" + proxy, "https": "http://" + proxy} if proxy else False
+        )
 
         start = time()
 
         mail_client = MailTm().get_account()
         mail_address = mail_client.address
 
-        client = Session(client_identifier='chrome110')
+        client = Session(client_identifier="chrome110")
         client.proxies = proxies
         client.headers = {
-            'origin': 'https://accounts.forefront.ai',
-            'user-agent': UserAgent().random,
+            "origin": "https://accounts.forefront.ai",
+            "user-agent": UserAgent().random,
         }
 
         response = client.post(
-            'https://clerk.forefront.ai/v1/client/sign_ups?_clerk_js_version=4.38.4',
-            data={'email_address': mail_address},
+            "https://clerk.forefront.ai/v1/client/sign_ups?_clerk_js_version=4.38.4",
+            data={"email_address": mail_address},
         )
 
         try:
-            trace_token = response.json()['response']['id']
+            trace_token = response.json()["response"]["id"]
             if logging:
                 print(trace_token)
         except KeyError:
-            return 'Failed to create account!'
+            return "Failed to create account!"
 
         response = client.post(
-            f'https://clerk.forefront.ai/v1/client/sign_ups/{trace_token}/prepare_verification?_clerk_js_version=4.38.4',
+            f"https://clerk.forefront.ai/v1/client/sign_ups/{trace_token}/prepare_verification?_clerk_js_version=4.38.4",
             data={
-                'strategy': 'email_link',
-                'redirect_url': 'https://accounts.forefront.ai/sign-up/verify'
+                "strategy": "email_link",
+                "redirect_url": "https://accounts.forefront.ai/sign-up/verify",
             },
         )
 
         if logging:
             print(response.text)
 
-        if 'sign_up_attempt' not in response.text:
-            return 'Failed to create account!'
+        if "sign_up_attempt" not in response.text:
+            return "Failed to create account!"
 
         while True:
             sleep(1)
             new_message: Message = mail_client.wait_for_message()
             if logging:
-                print(new_message.data['id'])
+                print(new_message.data["id"])
 
-            verification_url = re.findall(r'https:\/\/clerk\.forefront\.ai\/v1\/verify\?token=\w.+', new_message.text)[0]
+            verification_url = re.findall(
+                r"https:\/\/clerk\.forefront\.ai\/v1\/verify\?token=\w.+",
+                new_message.text,
+            )[0]
 
             if verification_url:
                 break
@@ -104,12 +110,14 @@ class Account:
 
         response = client.get(verification_url)
 
-        response = client.get('https://clerk.forefront.ai/v1/client?_clerk_js_version=4.38.4')
+        response = client.get(
+            "https://clerk.forefront.ai/v1/client?_clerk_js_version=4.38.4"
+        )
 
-        token = response.json()['response']['sessions'][0]['last_active_token']['jwt']
+        token = response.json()["response"]["sessions"][0]["last_active_token"]["jwt"]
 
-        with open('accounts.txt', 'a') as f:
-            f.write(f'{mail_address}:{token}\n')
+        with open("accounts.txt", "a") as f:
+            f.write(f"{mail_address}:{token}\n")
 
         if logging:
             print(time() - start)
@@ -122,11 +130,11 @@ class Completion:
     def create(
         token=None,
         chat_id=None,
-        prompt='',
-        action_type='new',
-        default_persona='607e41fe-95be-497e-8e97-010a59b2e2c0',
-        model='gpt-4',
-        proxy=None
+        prompt="",
+        action_type="new",
+        default_persona="607e41fe-95be-497e-8e97-010a59b2e2c0",
+        model="gpt-4",
+        proxy=None,
     ) -> ForeFrontResponse:
         """
         Create a completion request.
@@ -146,38 +154,40 @@ class Completion:
         Raises:
             Exception: If unable to get the response, please try again later.
         """
-        proxies = {'http': 'http://' + proxy, 'https': 'http://' + proxy} if proxy else None
+        proxies = (
+            {"http": "http://" + proxy, "https": "http://" + proxy} if proxy else None
+        )
 
         headers = {
-            'authority': 'chat-server.tenant-forefront-default.knative.chi.coreweave.com',
-            'accept': '*/*',
-            'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
-            'authorization': 'Bearer ' + token,
-            'cache-control': 'no-cache',
-            'content-type': 'application/json',
-            'origin': 'https://chat.forefront.ai',
-            'pragma': 'no-cache',
-            'referer': 'https://chat.forefront.ai/',
-            'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'user-agent': UserAgent().random,
+            "authority": "chat-server.tenant-forefront-default.knative.chi.coreweave.com",
+            "accept": "*/*",
+            "accept-language": "en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3",
+            "authorization": "Bearer " + token,
+            "cache-control": "no-cache",
+            "content-type": "application/json",
+            "origin": "https://chat.forefront.ai",
+            "pragma": "no-cache",
+            "referer": "https://chat.forefront.ai/",
+            "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            "user-agent": UserAgent().random,
         }
 
         json_data = {
-            'text': prompt,
-            'action': action_type,
-            'parentId': chat_id,
-            'workspaceId': chat_id,
-            'messagePersona': default_persona,
-            'model': model,
+            "text": prompt,
+            "action": action_type,
+            "parentId": chat_id,
+            "workspaceId": chat_id,
+            "messagePersona": default_persona,
+            "model": model,
         }
 
         response = post(
-            'https://chat-server.tenant-forefront-default.knative.chi.coreweave.com/chat',
+            "https://chat-server.tenant-forefront-default.knative.chi.coreweave.com/chat",
             headers=headers,
             proxies=proxies,
             json=json_data,
@@ -185,24 +195,31 @@ class Completion:
 
         if response.status_code == 200:
             data = response.json()
-            token = data['choices'][0]['delta'].get('content')
+            token = data["choices"][0]["delta"].get("content")
 
             if token is not None:
                 final_response = ForeFrontResponse(
                     **{
-                        'id': chat_id,
-                        'object': 'text_completion',
-                        'created': int(time()),
-                        'text': token,
-                        'model': model,
-                        'choices': [{'text': token, 'index': 0, 'logprobs': None, 'finish_reason': 'stop'}],
-                        'usage': {
-                            'prompt_tokens': len(prompt),
-                            'completion_tokens': len(token),
-                            'total_tokens': len(prompt) + len(token),
+                        "id": chat_id,
+                        "object": "text_completion",
+                        "created": int(time()),
+                        "text": token,
+                        "model": model,
+                        "choices": [
+                            {
+                                "text": token,
+                                "index": 0,
+                                "logprobs": None,
+                                "finish_reason": "stop",
+                            }
+                        ],
+                        "usage": {
+                            "prompt_tokens": len(prompt),
+                            "completion_tokens": len(token),
+                            "total_tokens": len(prompt) + len(token),
                         },
                     }
                 )
                 return final_response
 
-        raise Exception('Unable to get the response, please try again later.')
+        raise Exception("Unable to get the response, please try again later.")
