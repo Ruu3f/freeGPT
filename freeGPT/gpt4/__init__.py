c625393d8debcfcefdf91beccabfdfd5a8f17efe
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-05-25 20:06:48 +0530

    Update __init__.py

--- freeGPT/gpt4/__init__.py
+++ freeGPT/gpt4/__init__.py
@@ -3,6 +3,10 @@ import json
 
 
 class Completion:
+    """
+    A class for making requests to the AI completion API and retrieving responses.
+    """
+
     headers = {
         "authority": "ai.usesless.com",
         "accept": "application/json, text/plain, */*",
@@ -22,8 +26,21 @@ class Completion:
         presence_penalty: float = 1,
         temperature: float = 1,
     ):
+        """
+        Creates a request to the AI completion API with the specified parameters.
+
+        Args:
+            prompt (str): The text prompt for the AI model.
+            systemMessage (str): The system message for the conversation.
+            parentMessageId (str): The ID of the parent message.
+            presence_penalty (float): Controls the level of randomness in the AI model's responses based on previous messages.
+            temperature (float): Controls the randomness of the AI model's responses.
+
+        Returns:
+            Response: An object containing the API response.
+        """
         print(parentMessageId, prompt)
-        
+
         json_data = {
             "openaiKey": "",
             "prompt": prompt,
@@ -41,16 +58,27 @@ class Completion:
         url = "https://ai.usesless.com/api/chat-process"
         request = requests.post(url, headers=Completion.headers, json=json_data)
         content = request.content
-        
+
         class Response:
+            """
+            Represents the response from the AI completion API.
+            """
+
             def __init__(self, content):
                 self.content = content
 
             @property
             def text(self):
+                """
+                Get the text content of the response.
+
+                Returns:
+                    str: The text content of the response.
+                """
                 return self.content.decode("utf-8")
 
         return Response(content)
+
 
 if response.text is None:
     raise Exception("Unable to get the response, please try again later.")
