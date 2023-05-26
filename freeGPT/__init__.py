07e00f71211bf59568c51efe9736a4dca3744958
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-05-26 18:08:40 +0530

    Update __init__.py

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -3,15 +3,35 @@ from freeGPT import gpt3, gpt4
 
 __author__ = "Ruu3f"
 __version__ = "1.1.1"
+__all__ = ["Provider", "Completion"]
+
 
 class Provider(Enum):
-    GPT3 = 'gpt3'
-    GPT4 = 'gpt4'
+    """Enum class representing the available GPT providers."""
+
+    GPT3 = "gpt3"
+    GPT4 = "gpt4"
 
 
 class Completion:
+    """Class for generating completions using different GPT providers."""
+
     @staticmethod
     def create(provider: Provider, prompt: str, **kwargs) -> str:
+        """
+        Generates a completion using the specified provider.
+
+        Args:
+            provider (Provider): The GPT provider to use.
+            prompt (str): The prompt text for completion.
+            **kwargs: Additional keyword arguments specific to the provider.
+
+        Returns:
+            str: The generated completion text.
+
+        Raises:
+            Exception: If the provider doesn't exist.
+        """
         if provider == Provider.GPT3:
             return Completion.__gpt3_service(prompt, **kwargs)
         elif provider == Provider.GPT4:
@@ -21,9 +41,27 @@ class Completion:
 
     @staticmethod
     def __gpt3_service(prompt: str) -> str:
+        """
+        Generates a completion using the GPT-3 provider.
+
+        Args:
+            prompt (str): The prompt text for completion.
+
+        Returns:
+            str: The generated completion text.
+        """
         resp = gpt3.Completion.create(prompt=prompt)
-        return resp['text']
+        return resp["text"]
 
     @staticmethod
     def __gpt4_service(prompt: str) -> str:
+        """
+        Generates a completion using the GPT-4 provider.
+
+        Args:
+            prompt (str): The prompt text for completion.
+
+        Returns:
+            str: The generated completion text.
+        """
         return gpt4.Completion.create(prompt=prompt).text
