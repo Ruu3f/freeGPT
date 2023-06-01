6d9a9d0c309ff6b2b3eecb8ca006c5e89135d3e1
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-06-01 13:26:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="freeGPT",
-    version="1.1.4",
+    version="1.1.5",
     description="freeGPT is a Python package that gives free access to GPT3 and GPT4 and more models.",
     long_description=README,
     long_description_content_type="text/markdown",
