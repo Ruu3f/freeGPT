ca46221e1c9fdfadf36c77edc0bce9985863e594
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-06-26 00:44:37 +0530

    Update c_a_l.py

--- freeGPT/c_a_l.py
+++ freeGPT/c_a_l.py
@@ -22,6 +22,31 @@ class Completion:
         detailed: bool = False,
         proxies: Optional[str] = None,
     ) -> dict:
+        """
+        Create a completion.
+
+        Args:
+            prompt (str): The prompt for the completion.
+            page (int): The page number for the search results. Default is 1.
+            count (int): The number of results to return. Default is 10.
+            safe_search (str): The safe search setting. Default is "Off".
+            on_shopping_page (bool): Whether the search is on a shopping page. Default is False.
+            mkt (str): The market code for the search. Default is an empty string.
+            response_filter (str): The response filter for the search. Default is "WebPages, Translations, Computation, RelatedSearches".
+            domain (str): The domain for the search. Default is "youchat".
+            query_trace_id (str, optional): The query trace ID. If not provided, a new UUID will be generated. Default is None.
+            chat (List[str], optional): A list of chat messages. Default is None.
+            include_links (bool): Whether to include third-party search links in the results. Default is False.
+            detailed (bool): Whether to include detailed information in the results. Default is False.
+            proxies (str, optional): Proxies to use for the request. Default is None.
+
+        Returns:
+            dict: The completion result as a dictionary.
+
+        Raises:
+            Exception: If unable to fetch the response.
+        """
+
         if chat is None:
             chat = []
 
