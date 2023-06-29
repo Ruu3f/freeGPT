9c667dcc5e267906b29ee94606548e0dee3a365f
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-06-30 01:22:18 +0530

    Update: Version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="freeGPT",
-    version="1.2.0",
+    version="1.2.1",
     description="freeGPT provides access to GPT and other models for free.",
     long_description=README,
     long_description_content_type="text/markdown",
