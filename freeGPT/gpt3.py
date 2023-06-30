5c1d768792e8d58004e8422a6779a89b18c77043
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-07-01 02:44:25 +0530

    Fix: GPT-4 to no prompt, optimized

--- freeGPT/gpt3.py
+++ freeGPT/gpt3.py
@@ -3,68 +3,54 @@ import requests
 
 
 class Completion:
-    """
-    A class that interacts with the Ava API to generate completions for prompts.
-    """
-
     @staticmethod
     def create(prompt):
         """
-        Generates a completion based on the given prompt.
+        Create a new completion based on the given prompt.
 
         Args:
-            prompt (str): The prompt text to generate a completion for.
+            prompt (str): The prompt to generate a completion for.
 
         Returns:
-            str: The generated completion response.
+            str: The generated completion.
 
         Raises:
-            Exception: If there is an error while fetching the response from the API.
+            Exception: If unable to fetch the response.
         """
         try:
-            resp = ""
-            with requests.post(
+            session = requests.Session()
+            resp_obj = session.post(
                 "https://ava-alpha-api.codelink.io/api/chat",
                 headers={"Content-Type": "application/json"},
-                data=json.dumps(
-                    {
-                        "model": "gpt-4",
-                        "temperature": 0.7,
-                        "stream": True,
-                        "messages": [
-                            {
-                                "role": "system",
-                                "content": "You are Ava, an AI assistant. You are running on GPT-4 by OpenAI.",
-                            },
-                            {"role": "user", "content": prompt},
-                        ],
-                    }
-                ),
-                stream=True,
-            ) as resp_obj:
-                resp_obj.raise_for_status()
-                for line in resp_obj.iter_lines():
-                    line_text = line.decode("utf-8").strip()
-                    if line_text.startswith("data:"):
-                        data = line_text[len("data:") :]
-                        try:
-                            data_json = json.loads(data)
-                            if "choices" in data_json:
-                                choices = data_json["choices"]
-                                for choice in choices:
-                                    if (
-                                        "finish_reason" in choice
-                                        and choice["finish_reason"] == "stop"
-                                    ):
-                                        break
-                                    if (
-                                        "delta" in choice
-                                        and "content" in choice["delta"]
-                                    ):
-                                        resp += choice["delta"]["content"]
-                        except json.JSONDecodeError:
-                            pass
-        except requests.exceptions.RequestException:
+                json={
+                    "model": "gpt-4",
+                    "temperature": 0.6,
+                    "stream": True,
+                    "messages": [
+                        {"role": "system", "content": "You are Ava, an AI assistant."},
+                        {"role": "user", "content": prompt},
+                    ],
+                },
+            )
+        except:
             raise Exception("Unable to fetch the response.")
-
+        resp = ""
+        for line in resp_obj.iter_lines():
+            line_text = line.decode("utf-8").strip()
+            if line_text.startswith("data:"):
+                data = line_text[len("data:") :]
+                try:
+                    data_json = json.loads(data)
+                    if "choices" in data_json:
+                        choices = data_json["choices"]
+                        for choice in choices:
+                            if (
+                                "finish_reason" in choice
+                                and choice["finish_reason"] == "stop"
+                            ):
+                                break
+                            if "delta" in choice and "content" in choice["delta"]:
+                                resp += choice["delta"]["content"]
+                except json.JSONDecodeError:
+                    pass
         return resp
