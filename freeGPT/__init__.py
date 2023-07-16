1b0249d2b9bc3c3ab1411fa795d0da66225d95a1
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-07-16 23:58:01 +0530

    Updated version & added `prodia` to __all__ list

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -1,5 +1,5 @@
-from freeGPT import gpt3, gpt4, alpaca_7b
+from freeGPT import gpt3, gpt4, alpaca_7b, prodia
 
 __author__ = "Ruu3f"
-__version__ = "1.2.3"
-__all__ = [gpt3, gpt4, alpaca_7b]
+__version__ = "1.2.4"
+__all__ = [gpt3, gpt4, alpaca_7b, prodia]
