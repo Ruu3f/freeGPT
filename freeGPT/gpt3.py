3f8b4abb22e90dc332653945a4ac13a02b0d328d
ray <78753690+Ruu3f@users.noreply.github.com>
2023-07-27 01:09:55 +0530

    Using `aiohttp` instead of `requests`

--- freeGPT/gpt3.py
+++ freeGPT/gpt3.py
@@ -1,5 +1,5 @@
+from aiohttp import ClientSession, ClientError
 from json import loads, JSONDecodeError
-from requests import post, exceptions
 
 
 class Completion:
@@ -7,7 +7,8 @@ class Completion:
     This class provides methods for generating completions based on prompts.
     """
 
-    async def create(prompt):
+    @classmethod
+    async def create(cls, prompt):
         """
         Create a new completion based on the given prompt.
 
@@ -21,40 +22,46 @@ class Completion:
             Exception: If unable to fetch the response.
         """
         try:
-            resp_obj = post(
-                "https://ava-alpha-api.codelink.io/api/chat",
-                headers={"Content-Type": "application/json"},
-                json={
-                    "model": "gpt-4",
-                    "temperature": 0.6,
-                    "stream": True,
-                    "messages": [
-                        {"role": "system", "content": "You are Ava, an AI assistant."},
-                        {"role": "user", "content": prompt},
-                    ],
-                },
-                timeout=30,
-            )
-        except exceptions.RequestException:
+            async with ClientSession() as session:
+                async with session.post(
+                    "https://ava-alpha-api.codelink.io/api/chat",
+                    headers={"Content-Type": "application/json"},
+                    json={
+                        "model": "gpt-4",
+                        "temperature": 0.6,
+                        "stream": True,
+                        "messages": [
+                            {
+                                "role": "system",
+                                "content": "You are Ava, an AI assistant.",
+                            },
+                            {"role": "user", "content": prompt},
+                        ],
+                    },
+                    timeout=45,
+                ) as resp_obj:
+                    resp = ""
+                    async for line in resp_obj.content:
+                        line_text = line.decode("utf-8").strip()
+                        if line_text.startswith("data:"):
+                            data = line_text.split("data:")[1]
+                            try:
+                                data_json = loads(data)
+                                if "choices" in data_json:
+                                    choices = data_json["choices"]
+                                    for choice in choices:
+                                        if (
+                                            "finish_reason" in choice
+                                            and choice["finish_reason"] == "stop"
+                                        ):
+                                            break
+                                        if (
+                                            "delta" in choice
+                                            and "content" in choice["delta"]
+                                        ):
+                                            resp += choice["delta"]["content"]
+                            except JSONDecodeError:
+                                pass
+                    return resp
+        except ClientError:
             raise Exception("Unable to fetch the response.")
-
-        resp = ""
-        for line in resp_obj.iter_lines():
-            line_text = line.decode("utf-8").strip()
-            if line_text.startswith("data:"):
-                data = line_text.split("data:")[1]
-                try:
-                    data_json = loads(data)
-                    if "choices" in data_json:
-                        choices = data_json["choices"]
-                        for choice in choices:
-                            if (
-                                "finish_reason" in choice
-                                and choice["finish_reason"] == "stop"
-                            ):
-                                break
-                            if "delta" in choice and "content" in choice["delta"]:
-                                resp += choice["delta"]["content"]
-                except JSONDecodeError:
-                    pass
-        return resp
