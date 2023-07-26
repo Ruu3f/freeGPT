d71b3f91d8de9b333f5304c50484800c5301d28c
ray <78753690+Ruu3f@users.noreply.github.com>
2023-07-27 01:03:50 +0530

    Using random instead of specifying a seed manually

--- freeGPT/prodia.py
+++ freeGPT/prodia.py
@@ -1,6 +1,5 @@
-from io import BytesIO
+from random import randint
 from aiohttp import ClientSession
-from fake_useragent import UserAgent
 
 
 class Generation:
@@ -12,7 +11,7 @@ class Generation:
             prompt (str): The prompt for generating the image.
 
         Returns:
-            BytesIO: The generated image content as a BytesIO object.
+            resp: The generated image content
         """
         params = {
             "new": "true",
@@ -21,7 +20,7 @@ class Generation:
             "negative_prompt": "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.5),cross-eyed,sketches, (worst quality:2), (low quality:2.1), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair",
             "steps": "50",
             "cfg": "9.5",
-            "seed": "84187",
+            "seed": randint(1, 10000),
             "sampler": "Euler",
             "aspect_ratio": "square",
         }
@@ -39,14 +38,17 @@ class Generation:
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-site",
             "sec-gpc": "1",
-            "user-agent": UserAgent().random,
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36",
         }
 
         async with ClientSession() as session:
             async with session.get(
-                "https://api.prodia.com/generate", params=params, headers=headers
-            ) as response:
-                data = await response.json()
+                "https://api.prodia.com/generate",
+                params=params,
+                headers=headers,
+                timeout=45,
+            ) as resp:
+                data = await resp.json()
                 job_id = data["job"]
                 while True:
                     async with session.get(
@@ -58,5 +60,4 @@ class Generation:
                                 f"https://images.prodia.xyz/{job_id}.png?download=1",
                                 headers=headers,
                             ) as resp:
-                                content = await resp.content.read()
-                                return BytesIO(content)
+                                return await resp.content.read()
