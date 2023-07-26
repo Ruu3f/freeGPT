cb0df524f7362059edf9d170e45f38e89e50a6d4
ray <78753690+Ruu3f@users.noreply.github.com>
2023-07-27 01:12:13 +0530

    Updated version, added `pollinations` to the `__all__` list

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -1,5 +1,5 @@
-from freeGPT import gpt3, gpt4, alpaca_7b, prodia
+from freeGPT import gpt3, gpt4, alpaca_7b, prodia, pollinations
 
 __author__ = "Ruu3f"
-__version__ = "1.2.4"
-__all__ = [gpt3, gpt4, alpaca_7b, prodia]
+__version__ = "1.2.5"
+__all__ = [gpt3, gpt4, alpaca_7b, prodia, pollinations]
