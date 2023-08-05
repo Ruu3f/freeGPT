7d7ea3fc3559664f1f5e45f4511a224854020a37
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-08-05 14:51:17 +0530

    Updated version

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -1,5 +1,5 @@
 from freeGPT import gpt3, gpt4, alpaca_7b, prodia, pollinations
 
 __author__ = "Ruu3f"
-__version__ = "1.2.6"
+__version__ = "1.2.7"
 __all__ = [gpt3, gpt4, alpaca_7b, prodia, pollinations]
