56cc2f94675f476cb903c9c2d50a5356d10b682f
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-08-16 22:47:44 +0530

    Updated version, added `falcon_40b` to the imports and the __all__ list

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -1,5 +1,5 @@
-from freeGPT import gpt3, gpt4, alpaca_7b, prodia, pollinations
+from freeGPT import gpt3, gpt4, alpaca_7b, falcon_40b, prodia, pollinations
 
 __author__ = "Ruu3f"
-__version__ = "1.2.7"
-__all__ = [gpt3, gpt4, alpaca_7b, prodia, pollinations]
+__version__ = "1.2.8"
+__all__ = [gpt3, gpt4, alpaca_7b, falcon_40b, prodia, pollinations]
