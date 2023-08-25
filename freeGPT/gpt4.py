2220acc0fc0949626555b32ca62e45735d502894
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-08-25 23:08:35 +0530

    Simplified, using `curl-cffi` instead of `tls-client`

--- freeGPT/gpt4.py
+++ freeGPT/gpt4.py
@@ -2,15 +2,9 @@
 freeGPT's gpt4 module
 """
 
-from os import system
 from uuid import uuid4
 from re import findall
-
-try:
-    from tls_client import Session
-except ModuleNotFoundError:
-    system("pip install tls_client --no-cache-dir")
-    from tls_client import Session
+from curl_cffi.requests import get, RequestsError
 
 
 class Completion:
@@ -20,30 +14,23 @@ class Completion:
 
     async def create(self, prompt):
         """
-        Create a new completion session.
+        Generate a completion based on the provided prompt.
 
         Args:
-            prompt (str): The input prompt for generating text.
+            prompt (str): The input prompt to generate a completion from.
 
         Returns:
-            str: Generated text based on the provided prompt.
+            str: The generated completion as a text string.
 
         Raises:
-            Exception: If unable to fetch a valid response.
+            Exception: If the response does not contain the expected "youChatToken".
         """
-        client = Session(client_identifier="chrome_108")
-        client.headers = {
+        headers = {
             "authority": "you.com",
             "accept": "text/event-stream",
             "accept-language": "en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3",
             "cache-control": "no-cache",
             "referer": "https://you.com/search?q=who+are+you&tbm=youchat",
-            "sec-ch-ua": '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
             "cookie": f"safesearch_guest=Off; uuid_guest={str(uuid4())}",
             "user-agent": "Mozilla/5.0 (Windows NT 5.1; U;  ; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.52",
         }
@@ -59,11 +46,14 @@ class Completion:
             "queryTraceId": str(uuid4()),
             "chat": [],
         }
-        resp = client.get(
-            "https://you.com/api/streamingSearch", params=params, timeout_seconds=30
+        resp = get(
+            "https://you.com/api/streamingSearch",
+            headers=headers,
+            params=params,
+            impersonate="chrome107",
         )
         if "youChatToken" not in resp.text:
-            raise Exception("Unable to fetch response.")
+            raise RequestsError("Unable to fetch response.")
         return (
             "".join(findall(r"{\"youChatToken\": \"(.*?)\"}", resp.text))
             .replace("\\n", "\n")
