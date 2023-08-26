50ed6eac09a2a3fbd224f2825e14605e7889eec7
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-08-26 23:02:53 +0530

    Fixed (Again)

--- freeGPT/__init__.py
+++ freeGPT/__init__.py
@@ -1,4 +1,4 @@
-import gpt3, gpt4, alpaca_7b, falcon_40b, prodia, pollinations
+from freeGPT import gpt3, gpt4, alpaca_7b, falcon_40b, prodia, pollinations
 
 __author__ = "Ruu3f"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
