6beee0088bb34dba2daebebe66b46697b3642836
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-08-29 15:59:54 +0530

    Fixed

--- freeGPT/gpt3.py
+++ freeGPT/gpt3.py
@@ -2,7 +2,6 @@
 freeGPT's gpt3 module
 """
 
-
 from aiohttp import ClientSession, ClientError
 
 
@@ -16,24 +15,26 @@ class Completion:
         Create a completion for the given prompt using an AI text generation API.
 
         Args:
-            prompt (str): The prompt for which completion needs to be generated.
+            prompt (str): The input prompt for generating the text.
 
         Returns:
-            str: The generated completion text.
+            str: The generated text as a response from the API.
 
         Raises:
-            ClientError: If there's an issue with the API request or response.
+            ClientError: If there is an issue with sending the request or fetching the response.
         """
         async with ClientSession() as session:
             try:
                 async with session.post(
-                    url="https://api.aichatos.cloud/api/generateStream",
-                    headers={"origin": "https://chat9.yqcloud.top"},
+                    url="https://api.binjie.fun/api/generateStream",
+                    headers={
+                        "origin": "https://chat.jinshutuan.com",
+                        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36",
+                    },
                     json={
-                        "prompt": f"Always respond in English. Prompt: {prompt}",
-                        "network": True,
-                        "system": "",
-                        "withoutContext": False,
+                        "prompt": prompt,
+                        "system": "Always talk in English.",
+                        "withoutContext": True,
                         "stream": False,
                     },
                 ) as resp:
