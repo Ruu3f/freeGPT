0bfd44d4a1fe62ad9096b6453c752f0a66bb4967
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-09-11 18:51:53 +0530

    Cleaned up

--- freeGPT/gpt4.py
+++ freeGPT/gpt4.py
@@ -25,27 +25,25 @@ class Completion:
         Raises:
             Exception: If the response does not contain the expected "youChatToken".
         """
-        headers = {
-            "cache-control": "no-cache",
-            "referer": "https://you.com/search?q=gpt4&tbm=youchat",
-            "cookie": f"safesearch_guest=Off; uuid_guest={str(uuid4())}",
-        }
-        params = {
-            "q": prompt,
-            "page": 1,
-            "count": 10,
-            "safeSearch": "Off",
-            "onShoppingPage": False,
-            "mkt": "",
-            "responseFilter": "WebPages,Translations,TimeZone,Computation,RelatedSearches",
-            "domain": "youchat",
-            "queryTraceId": str(uuid4()),
-            "chat": [],
-        }
         resp = get(
             "https://you.com/api/streamingSearch",
-            headers=headers,
-            params=params,
+            headers={
+                "cache-control": "no-cache",
+                "referer": "https://you.com/search?q=gpt4&tbm=youchat",
+                "cookie": f"safesearch_guest=Off; uuid_guest={str(uuid4())}",
+            },
+            params={
+                "q": prompt,
+                "page": 1,
+                "count": 10,
+                "safeSearch": "Off",
+                "onShoppingPage": False,
+                "mkt": "",
+                "responseFilter": "WebPages,Translations,TimeZone,Computation,RelatedSearches",
+                "domain": "youchat",
+                "queryTraceId": str(uuid4()),
+                "chat": [],
+            },
             impersonate="chrome107",
         )
         if "youChatToken" not in resp.text:
--- freeGPT/prodia.py
+++ freeGPT/prodia.py
@@ -21,17 +21,6 @@ class Generation:
         Returns:
             resp: The generated image content
         """
-        params = {
-            "new": "true",
-            "prompt": prompt,
-            "model": "dreamshaper_6BakedVae.safetensors [114c8abb]",
-            "negative_prompt": "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.5),cross-eyed,sketches, (worst quality:2), (low quality:2.1), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair",
-            "steps": "50",
-            "cfg": "9.5",
-            "seed": randint(1, 10000),
-            "sampler": "Euler",
-            "aspect_ratio": "square",
-        }
         headers = {
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36",
         }
@@ -39,7 +28,17 @@ class Generation:
             async with ClientSession() as session:
                 async with session.get(
                     "https://api.prodia.com/generate",
-                    params=params,
+                    params={
+                        "new": "true",
+                        "prompt": prompt,
+                        "model": "dreamshaper_6BakedVae.safetensors [114c8abb]",
+                        "negative_prompt": "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.5),cross-eyed,sketches, (worst quality:2), (low quality:2.1), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair",
+                        "steps": "50",
+                        "cfg": "9.5",
+                        "seed": randint(1, 10000),
+                        "sampler": "Euler",
+                        "aspect_ratio": "square",
+                    },
                     headers=headers,
                     timeout=30,
                 ) as resp:
